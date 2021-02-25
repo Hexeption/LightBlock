c839b5d53fdffe7724c7740b3ed9410e9f88dbf5
Hexeption <minecraftfun201@gmail.com>
2021-02-25 19:59:46 +0000

    Updated to 1.16.5

--- src/main/java/dev/hexeption/lightblock/mixin/MixinClientWorld.java
+++ src/main/java/dev/hexeption/lightblock/mixin/MixinClientWorld.java
@@ -18,14 +18,20 @@
 
 package dev.hexeption.lightblock.mixin;
 
-import dev.hexeption.lightblock.block.BlockLight;
-import dev.hexeption.lightblock.registry.LBlocks;
-import dev.hexeption.lightblock.registry.LParticles;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
+
+import dev.hexeption.lightblock.block.BlockLight;
+import dev.hexeption.lightblock.registry.LBlocks;
+import dev.hexeption.lightblock.registry.LParticles;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
@@ -36,12 +42,9 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.MutableWorldProperties;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
 /**
  * MixinClientWorld
@@ -74,24 +77,24 @@ public abstract class MixinClientWorld extends World {
         BlockState blockState = this.getBlockState(pos);
         assert this.client.player != null;
         this.client.player.getItemsHand().forEach(itemStack -> {
-            if (itemStack.isItemEqual(LBlocks.blockLight_0.asItem().getStackForRender()) || itemStack.isItemEqual(LBlocks.blockLight_1.asItem().getStackForRender()) ||
-                itemStack.isItemEqual(LBlocks.blockLight_2.asItem().getStackForRender()) || itemStack.isItemEqual(LBlocks.blockLight_3.asItem().getStackForRender()) ||
-                itemStack.isItemEqual(LBlocks.blockLight_4.asItem().getStackForRender()) || itemStack.isItemEqual(LBlocks.blockLight_5.asItem().getStackForRender()) ||
-                itemStack.isItemEqual(LBlocks.blockLight_6.asItem().getStackForRender()) || itemStack.isItemEqual(LBlocks.blockLight_7.asItem().getStackForRender()) ||
-                itemStack.isItemEqual(LBlocks.blockLight_8.asItem().getStackForRender()) || itemStack.isItemEqual(LBlocks.blockLight_9.asItem().getStackForRender()) ||
-                itemStack.isItemEqual(LBlocks.blockLight_10.asItem().getStackForRender()) || itemStack.isItemEqual(LBlocks.blockLight_11.asItem().getStackForRender()) ||
-                itemStack.isItemEqual(LBlocks.blockLight_12.asItem().getStackForRender()) || itemStack.isItemEqual(LBlocks.blockLight_13.asItem().getStackForRender()) ||
-                itemStack.isItemEqual(LBlocks.blockLight_14.asItem().getStackForRender()) || itemStack.isItemEqual(LBlocks.blockLight_15.asItem().getStackForRender())) {
+            if (itemStack.isItemEqual(LBlocks.blockLight_0.asItem().getDefaultStack()) || itemStack.isItemEqual(LBlocks.blockLight_1.asItem().getDefaultStack()) ||
+                    itemStack.isItemEqual(LBlocks.blockLight_2.asItem().getDefaultStack()) || itemStack.isItemEqual(LBlocks.blockLight_3.asItem().getDefaultStack()) ||
+                    itemStack.isItemEqual(LBlocks.blockLight_4.asItem().getDefaultStack()) || itemStack.isItemEqual(LBlocks.blockLight_5.asItem().getDefaultStack()) ||
+                    itemStack.isItemEqual(LBlocks.blockLight_6.asItem().getDefaultStack()) || itemStack.isItemEqual(LBlocks.blockLight_7.asItem().getDefaultStack()) ||
+                    itemStack.isItemEqual(LBlocks.blockLight_8.asItem().getDefaultStack()) || itemStack.isItemEqual(LBlocks.blockLight_9.asItem().getDefaultStack()) ||
+                    itemStack.isItemEqual(LBlocks.blockLight_10.asItem().getDefaultStack()) || itemStack.isItemEqual(LBlocks.blockLight_11.asItem().getDefaultStack()) ||
+                    itemStack.isItemEqual(LBlocks.blockLight_12.asItem().getDefaultStack()) || itemStack.isItemEqual(LBlocks.blockLight_13.asItem().getDefaultStack()) ||
+                    itemStack.isItemEqual(LBlocks.blockLight_14.asItem().getDefaultStack()) || itemStack.isItemEqual(LBlocks.blockLight_15.asItem().getDefaultStack())) {
                 if (blockState.getBlock() == LBlocks.blockLight_0 || blockState.getBlock() == LBlocks.blockLight_1 ||
-                    blockState.getBlock() == LBlocks.blockLight_2 || blockState.getBlock() == LBlocks.blockLight_3 ||
-                    blockState.getBlock() == LBlocks.blockLight_4 || blockState.getBlock() == LBlocks.blockLight_5 ||
-                    blockState.getBlock() == LBlocks.blockLight_6 || blockState.getBlock() == LBlocks.blockLight_7 ||
-                    blockState.getBlock() == LBlocks.blockLight_8 || blockState.getBlock() == LBlocks.blockLight_9 ||
-                    blockState.getBlock() == LBlocks.blockLight_10 || blockState.getBlock() == LBlocks.blockLight_11 ||
-                    blockState.getBlock() == LBlocks.blockLight_12 || blockState.getBlock() == LBlocks.blockLight_13 ||
-                    blockState.getBlock() == LBlocks.blockLight_14 || blockState.getBlock() == LBlocks.blockLight_15) {
+                        blockState.getBlock() == LBlocks.blockLight_2 || blockState.getBlock() == LBlocks.blockLight_3 ||
+                        blockState.getBlock() == LBlocks.blockLight_4 || blockState.getBlock() == LBlocks.blockLight_5 ||
+                        blockState.getBlock() == LBlocks.blockLight_6 || blockState.getBlock() == LBlocks.blockLight_7 ||
+                        blockState.getBlock() == LBlocks.blockLight_8 || blockState.getBlock() == LBlocks.blockLight_9 ||
+                        blockState.getBlock() == LBlocks.blockLight_10 || blockState.getBlock() == LBlocks.blockLight_11 ||
+                        blockState.getBlock() == LBlocks.blockLight_12 || blockState.getBlock() == LBlocks.blockLight_13 ||
+                        blockState.getBlock() == LBlocks.blockLight_14 || blockState.getBlock() == LBlocks.blockLight_15) {
                     switch (blockState.get(BlockLight.lightLevel)) {
-                        case 0:
+                    case 0:
                             this.addParticle(LParticles.lightParticle_0, (double) i + 0.5D, (double) j + 0.5D, (double) k + 0.5D, 0.0D, 0.0D, 0.0D);
                             break;
                         case 1:
